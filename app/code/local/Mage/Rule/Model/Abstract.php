a05dc87209746966c93e5458b1e1e4f5d362ece0
sition <micheldoens@hotmail.com>
2013-05-27 11:36:34 +0200

    negatief wel toestaan

--- app/code/local/Mage/Rule/Model/Abstract.php
+++ app/code/local/Mage/Rule/Model/Abstract.php
@@ -89,12 +89,16 @@ abstract class Mage_Rule_Model_Abstract extends Mage_Core_Model_Abstract
      */
     protected function _beforeSave()
     {
+        
+        // sition! negatief wel toestaan.
+    	
         // Check if discount amount not negative
-        if ($this->hasDiscountAmount()) {
-            if ((int)$this->getDiscountAmount() < 0) {
-                Mage::throwException(Mage::helper('rule')->__('Invalid discount amount.'));
-            }
-        }
+        
+//         if ($this->hasDiscountAmount()) {
+//             if ((int)$this->getDiscountAmount() < 0) {
+//                 Mage::throwException(Mage::helper('rule')->__('Invalid discount amount.'));
+//             }
+//         }
 
         // Serialize conditions
         if ($this->getConditions()) {
